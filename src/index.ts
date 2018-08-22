8e727def8804906226be90a2620754f898e4ef07
Mkalo <matheus_belo@id.uff.br>
2018-08-22 10:18:28 -0300

    Added frameworks and tools
    Electron
    Pixi.JS
    TSLint
    gulp.js
    Webpack

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log('Hello World!');
+import { app as ElectronApp, BrowserWindow } from 'electron';
+
+let mainWindow: Electron.BrowserWindow;
+
+ElectronApp.on('ready', () => {
+	mainWindow = new BrowserWindow({ width: 800, height: 600, resizable: false, useContentSize: true, webPreferences: { devTools: false } });
+	mainWindow.setMenu(null);
+	mainWindow.loadURL(`file://${__dirname}/app/index.html`);
+});
+
+ElectronApp.on('window-all-closed', () => {
+	ElectronApp.quit();
+});
